820b6f556aa650c1215ea88d04f3f8c15852c324
Igor Bertoldo Siqueira <igorbertoldo2011@hotmail.com>
2019-03-14 13:31:27 -0300

    smartTable

--- Projetos/smartTable/src/app/app.module.ts
+++ Projetos/smartTable/src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopoComponent } from './topo/topo.component';
-import { RodapeComponent } from './rodape/rodape.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TopoComponent,
-    RodapeComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

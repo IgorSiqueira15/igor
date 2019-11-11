8797978b474aecc3907e5624c0173c5c0c1a66d4
IgorSiqueira15 <igorbertoldo2011@hotmail.com>
2019-11-11 08:53:38 -0300

    criando componentes e inserindo primeng

--- Projetos/drag-drop/e2e/src/app.e2e-spec.ts
+++ Projetos/drag-drop/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('dragDrop app is running!');
+    expect(page.getTitleText()).toEqual('drag-drop app is running!');
   });
 
   afterEach(async () => {
--- Projetos/dragDrop/src/app/task.ts
+++
@@ -1,7 +0,0 @@
-export class Task {
-  id: number;
-  title: string;
-  descrition: string;
-  assignTo: string;
-  lastModifing: string;
-}

1ea6c9f09900ee9235d6ada033b9a090ea7e0bca
IgorSiqueira15 <igorbertoldo2011@hotmail.com>
2019-11-11 09:10:09 -0300

    Inserindo componente dashboard

--- Projetos/drag-drop/src/app/app-routing.module.ts
+++ Projetos/drag-drop/src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", loadChildren: () => import ("../app/core/core.module").then(m => m.CoreModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Projetos/drag-drop/src/app/app.module.ts
+++ Projetos/drag-drop/src/app/app.module.ts
@@ -5,15 +5,17 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]

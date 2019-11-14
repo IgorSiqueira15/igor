c182edb9ab65956ddfc401ea2a4f054d47245d38
Igor Bertoldo Siqueira <igor.siqueira@ftt-17L.engcomp.local>
2019-11-14 18:58:38 -0200

    inserindo funçao splice

--- Projetos/drag-drop/src/app/core/dashboard/dashboard.component.ts
+++ Projetos/drag-drop/src/app/core/dashboard/dashboard.component.ts
@@ -10,38 +10,54 @@ import { ModalComponent } from 'src/app/modal/modal.component';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
   items: Dashboard[];
 
   dropped = [];
 
+  fazer = [];
+  desenvolvendo = [];
+  testando = [];
+  feito = [];
+
   itemArrastado = null;
 
-  constructor(private coreService: CoreService,
-    protected modal: NgbModal) { }
+  constructor(private coreService: CoreService, protected modal: NgbModal) {}
 
   ngOnInit() {
-    this.coreService.findAll().then(item => this.items = item);
+    this.coreService.findAll().then(item => (this.items = item));
   }
 
   dragStart(event, item: Dashboard) {
     this.itemArrastado = item;
   }
-  dragEnd(event) {
-  }
+  dragEnd(event) {}
 
   drop(event) {
-    if(this.itemArrastado){
-      this.dropped.push(this.itemArrastado);
-      this.itemArrastado = null;
+    console.log(this.items);
+
+    if (this.itemArrastado) {
+      switch (
+        document.getElementsByTagName('p-panel')[1].getAttribute('header')
+      ) {
+        case 'Desenvolvimento':
+          this.desenvolvendo.push(this.itemArrastado);
+          this.items.splice(this.items.indexOf(this.itemArrastado), 1);
+          this.itemArrastado = null;
+          break;
+        case 'Testando':
+          this.testando.push(this.itemArrastado);
+          this.itemArrastado = null;
+          break;
+        case 'Feito':
+          this.feito.push(this.itemArrastado);
+          this.itemArrastado = null;
+          break;
+      }
     }
   }
 
   onModalItem() {
     const modal = this.modal.open(ModalComponent, { size: 'lg' });
-    modal.result.then(confirmado => {
-    });
+    modal.result.then(confirmado => {});
   }
-
-
 }

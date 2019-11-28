7b5e1aadec4a3ad5fb753e660f229cd1deae3d3d
Igor Bertoldo Siqueira <igor.siqueira@ftt-17L.engcomp.local>
2019-11-28 17:26:02 -0200

    Lendo arquivo .log

--- Projetos/quakeLogParser/src/app/app.component.ts
+++ Projetos/quakeLogParser/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'quakeLogParser';
+  title = "quakeLogParser";
+
+  fileLines: string[];
+
+  readFile($event): void {
+    this.readThis($event.target);
+  }
+
+  readThis(valueInput: any): void {
+    var file: File = valueInput.files[0];
+    var reader: FileReader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = e => {
+      this.logResult(reader.result);
+      // console.log(reader.result);
+    };
+  }
+
+  logResult(log) {
+    console.log("Testando: ", log);
+  }
 }

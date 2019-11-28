756b888a855b6ecbab98fa9ed4e3dfced391f469
Igor Bertoldo Siqueira <igor.siqueira@ftt-17L.engcomp.local>
2019-11-28 19:13:55 -0200

    Inserindo forma array dentro de array para contar as mortes

--- Projetos/quakeLogParser/src/app/app.component.ts
+++ Projetos/quakeLogParser/src/app/app.component.ts
@@ -9,6 +9,9 @@ export class AppComponent {
   title = "quakeLogParser";
 
   fileLines: string[];
+  games: number = -1;
+  deaths: number = 0;
+  numberLines: number = 0;
 
   readFile($event): void {
     this.readThis($event.target);
@@ -25,6 +28,28 @@ export class AppComponent {
   }
 
   logResult(log) {
-    console.log("Testando: ", log);
+    this.fileLines = log.split(/[0-9]?[0-9]?[0-9]:[0-9][0-9]/);
+    // console.log("Testando: \n" + this.fileLines);
+    let a = [];
+    let count = 0;
+    this.fileLines.forEach(line => {
+      this.numberLines++;
+      if (line.includes(" InitGame:")) {
+        this.games++;
+        count = 0;
+      }
+      if (line.includes("Kill")) {
+        count++;
+        a[this.games] = count;
+      }
+
+      if (this.numberLines >= 675 && this.numberLines <= 815) {
+        this.deaths++;
+      }
+    });
+    console.log("Quantidade de Mortos: " + this.deaths);
+    // console.log("Quantidade de Jogos: " + this.games);
+    // console.log("Numero de Linhas: " + this.numberLines);
+    console.log(a);
   }
 }
